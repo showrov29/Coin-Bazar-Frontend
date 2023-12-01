c4045d14e90dcbb4a98231c8061e6d290a726195
Showrov Islam <63032286+showrov29@users.noreply.github.com>
2023-12-01 22:55:44 +0600

    ui update

--- pages/components/Card.js
+++ pages/components/Card.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 export default function Card(props) {
-	const { id, name, rating, image, price } = props.product;
+	const { name, rating, price, image } = props;
 
 	return (
 		<div className="w-full h-96 max-w-sm  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 			<a href="#">
 				<img
-					className="p-8 rounded-t-lg hover:scale-105 transition-all duration-300"
+					className="p-6 rounded-t-lg hover:scale-105 transition-all duration-300 "
 					src={image}
 					alt="product image"
+					style={{ borderRadius: "15%" }}
 				/>
 			</a>
 			<div className="px-5 pb-5">
@@ -71,7 +72,7 @@ export default function Card(props) {
 					</span>
 					<a
 						href="#"
-						className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+						className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition-colors duration-300">
 						Add to cart
 					</a>
 				</div>
--- pages/home.js
+++ pages/home.js
@@ -2,7 +2,7 @@
 import React, { useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { applyDarkMode, toggleDarkMode } from "./utils/darkMode";
+import { applyDarkMode, toggleDarkMode } from "../utils/darkMode";
 import Products from "@/Data/data";
 import Card from "./components/Card";
 const Home = () => {
@@ -25,7 +25,15 @@ const Home = () => {
 
 				<div className="mx-8 grid grid-flow-row gap-5 grid-cols-5 xl:grid-cols-5  lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1">
 					{Products.map((product) => {
-						return <Card key={product.id} product={product} />;
+						return (
+							<Card
+								key={product.id}
+								name={product.name}
+								rating={product.rating}
+								price={product.price}
+								image={product.image}
+							/>
+						);
 					})}
 				</div>
 			</div>
