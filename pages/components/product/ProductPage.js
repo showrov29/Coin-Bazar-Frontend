23c710b0f353c3c9127e7858b96b3a01e4ee9cc2
injammahin <injammahin5507@gmail.com>
2023-12-14 15:24:46 +0600

    final_lab

--- pages/components/product/ProductPage.js
+++ pages/components/product/ProductPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Link from "next/link";
+import Header1 from "../Header1";
 
 const ProductPage = () => {
   return (
     <div className="">
+      <Header1 />
       <section class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto ">
           <div class="flex flex-wrap -m-4">
